4f237d3b038e25ee86f4240f812fc273c00b79c2
Trey <TreyRuffy@users.noreply.github.com>
2025-10-26 13:03:36 -0600

    feat(#173): Fix old f3 from showing and improve rendering

--- common/src/main/java/me/cominixo/betterf3/mixin/DebugMixin.java
+++ common/src/main/java/me/cominixo/betterf3/mixin/DebugMixin.java
@@ -1,15 +1,23 @@
 package me.cominixo.betterf3.mixin;
 
+import java.util.Collection;
+import java.util.List;
 import me.cominixo.betterf3.config.GeneralOptions;
 import net.minecraft.Util;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.DebugScreenOverlay;
+import net.minecraft.client.gui.components.debug.DebugEntryNoop;
+import net.minecraft.client.gui.components.debug.DebugScreenEntries;
+import net.minecraft.client.gui.components.debug.DebugScreenEntryList;
+import net.minecraft.resources.ResourceLocation;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import static me.cominixo.betterf3.utils.Utils.START_X_POS;
@@ -29,6 +37,12 @@ public abstract class DebugMixin {
   @Final
   @Shadow private Minecraft minecraft;
 
+  @Unique
+  private static final ResourceLocation BETTERF3_RESOURCE = DebugScreenEntries.register("betterf3", new DebugEntryNoop());
+
+  @Unique
+  private static final List<ResourceLocation> BETTERF3_LIST = List.of(BETTERF3_RESOURCE);
+
   /**
    * Ensures that the TPS graph works.
    *
@@ -41,6 +55,22 @@ public abstract class DebugMixin {
       return;
     }
     context.pose().pushMatrix();
+  }
+
+  @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/components/debug/DebugScreenEntryList;getCurrentlyEnabled()Ljava/util/Collection;"))
+  private Collection<ResourceLocation> currentlyEnabled(final DebugScreenEntryList instance) {
+    if (!GeneralOptions.disableMod && this.minecraft.debugEntries.isF3Visible()) {
+      return BETTERF3_LIST;
+    }
+    return instance.getCurrentlyEnabled();
+  }
+
+  @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/components/debug/DebugScreenEntryList;isF3Visible()Z"))
+  private boolean isF3Visible(final DebugScreenEntryList instance) {
+    if (!GeneralOptions.disableMod && this.minecraft.debugEntries.isF3Visible()) {
+      return false;
+    }
+    return instance.isF3Visible();
   }
 
   /**
@@ -63,7 +93,7 @@ public abstract class DebugMixin {
    * @param ci Callback info
    */
   @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/profiling/ProfilerFiller;push(Ljava/lang/String;)V"))
-  public void renderAnimation(final GuiGraphics context, final CallbackInfo ci) {
+  public synchronized void renderAnimation(final GuiGraphics context, final CallbackInfo ci) {
 
     if (GeneralOptions.disableMod) {
       return;
